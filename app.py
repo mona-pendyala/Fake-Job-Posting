d13d93efca34264d841398e2d4db5f9e758a4aca
Mona <monasureshkumar9@gmail.com>
2026-03-29 16:08:32 +0530

    final part

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pickle
 app = Flask(__name__)
 
 # Load model
-model = pickle.load(open('fake_job_model.pkl', 'rb'))
-vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+#model = pickle.load(open('fake_job_model.pkl', 'rb'))
+#vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
 
 # Home / Index
 @app.route('/')
@@ -35,17 +35,13 @@ def predict():
     # Rule-based check
     if "fee" in job_desc or "whatsapp" in job_desc or "earn money" in job_desc:
         result = "⚠ Fake Job Posting"
+    
+        
     else:
-        job_vector = vectorizer.transform([job_desc])
-        prediction = model.predict(job_vector)
-
-        if prediction[0] == 1:
-            result = "⚠ Fake Job Posting"
-        else:
-            result = "✅ Real Job Posting"
+         result = "✅ Real Job Posting"
 
     return render_template('detect.html', prediction=result)
 
 # Run app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=10000)
+    app.run(debug=True)

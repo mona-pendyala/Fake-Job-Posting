7599c2425bfa6d6f87b3203c2b80fe5c8a844309
Mona <monasureshkumar9@gmail.com>
2026-04-08 07:41:02 +0530

    mona

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Load model
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model = pickle.load(open(os.path.join(BASE_DIR,'fake_job_model.pkl'), 'rb'))
-vectorizer = pickle.load(open(os.path.join(BASE_DIR'vectorizer.pkl'), 'rb'))
+vectorizer = pickle.load(open(os.path.join(BASE_DIR,'vectorizer.pkl'), 'rb'))
 
 
 #signup
